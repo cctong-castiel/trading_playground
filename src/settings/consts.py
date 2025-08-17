21e1fa666ff4cf5bcef09740edd965bf3b8eb4e1
cctong-castiel <castieltong@gmail.com>
2025-08-17 15:32:21 +0800

    can show several graphs using multi-processing

--- src/settings/consts.py
+++ src/settings/consts.py
@@ -6,5 +6,8 @@ import yaml
 SHIFT_STOCHASTIC_VAL = 200
 SHIFT_RSI_VAL = 100
 
-with open(os.path.join(os.getcwd(), 'config', 'indicators_config.yaml'), 'r', encoding='utf-8') as f:
+PROJECT_DIR = os.getcwd()
+PROJECT_DIR = '/Users/tongcc/dev/projects/trading'
+
+with open(os.path.join(PROJECT_DIR, 'config', 'indicators_config.yaml'), 'r', encoding='utf-8') as f:
     indicator_config = yaml.safe_load(f)
